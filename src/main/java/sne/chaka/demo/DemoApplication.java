3e6d7888dd47d8121ccf97475c127229ccd460c9
Malick DIOP <diop-malick@hotmail.fr>
2018-08-08 15:56:16 +0000

    Refactor

--- src/main/java/sne/chaka/demo/DemoApplication.java
+++ src/main/java/sne/chaka/demo/DemoApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import sne.chaka.demo.domain.User;
 import sne.chaka.demo.domain.repository.UserRepository;
 
 @SpringBootApplication
@@ -22,11 +20,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
+	/**
+	 * List Spring Beanprovided by Boot
+	 */
+	// @Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx, UserRepository userRepository) {
 		return args -> {
-
-			// Beans list
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			log.info("Let's inspect the beans provided by Spring Boot:");
@@ -34,7 +33,6 @@ public class DemoApplication {
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
-
 		};
 	}
 
--- src/test/java/sne/chaka/demo/UserRepositoryTest.java
+++ src/test/java/sne/chaka/demo/UserRepositoryTest.java
@@ -3,6 +3,8 @@ package sne.chaka.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +23,13 @@ public class UserRepositoryTest {
 
 	@Test
 	public void findByLastName() throws Exception {
+		// given
 		User savedUser = userRepository.save(new User("Stephan", "Nicolle"));
-		assertThat(userRepository.findById(1L).equals(savedUser.getId()));
+		
+		// when 
+		Optional<User> retrievedUser = userRepository.findById(1L);
+		
+		// Then
+		assertThat(retrievedUser.get().getId().equals(savedUser.getId()));
 	}
 }
